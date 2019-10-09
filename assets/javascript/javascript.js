cf54a9bbbce865d87c007ccec744d2772a250824
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2019-10-09 10:07:33 -0700

    first html and jschanges

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // generate computer random number 
+    var computerRandomNumber = Math.floor(Math.random()* 120);
+
+    // generate crystal random numbers
+    var crystalOne = 
+    var crystalTwo = 
+    var crystalThree = 
+    var crystalFour = 
+});
